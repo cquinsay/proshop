79a8f52d153f2f4bbbc01961a036685d81f855a6
Charity Quinsay <cquinsay@gmail.com>
2022-02-01 13:51:49 -0800

    created product screen

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 const Rating = ({ value, text, color }) => {
     return <div className='rating'>
@@ -24,6 +26,12 @@ const Rating = ({ value, text, color }) => {
 
 Rating.defaultProps = {
     color: '#f8e825',
+}
+
+Rating.propTypes = {
+    value: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string,
 }
 
 export default Rating;
