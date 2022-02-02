23837ee89c2a6ce9d9d039e71c5350092bb144aa
Charity Quinsay <cquinsay@gmail.com>
2022-02-01 16:57:53 -0800

    created user, product and order models

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,10 +6,10 @@ const connectDB = async () => {
 
         })
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
 
     } catch (error) {
-        console.error(`Error: ${error.message}`)
+        console.error(`Error: ${error.message}`.red.underline.bold)
         process.exit(1)
 
     }
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
 import connectDB from './config/db.js';
 import products from './data/products.js';
+
 
 
 dotenv.config()
@@ -27,4 +29,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 3001
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
