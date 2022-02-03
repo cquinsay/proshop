d1d2e083a931493d4c69b9d9843aec0c136cc839
Charity Quinsay <cquinsay@gmail.com>
2022-02-02 22:42:22 -0800

    removed serviceWorker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import './bootstrap.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <Provider store={store}>
