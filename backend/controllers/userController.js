7bed41e97910af6bb76b3533b98789208ccf2079
Charity Quinsay <cquinsay@gmail.com>
2022-02-08 14:15:23 -0800

    created authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,12 +19,30 @@ const authUser = asyncHandler(async (req, res) => {
       isAdmin: user.isAdmin,
       token: generateToken(user._id),
     })
-
   } else {
     res.status(401)
     throw new Error('Invalid email or password')
   }
-
 })
 
-export { authUser }
+// @desc    GET user profile
+// @route   POST /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile }
