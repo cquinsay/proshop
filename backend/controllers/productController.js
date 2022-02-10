cc4d50d2226d2f8a1c03ea99f616456161f91778
Charity Quinsay <cquinsay@gmail.com>
2022-02-10 13:57:16 -0800

    created order controllers and order routes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,7 +3,7 @@ import Product from '../models/productModel.js';
 
 
 // @desc    Fetch all products
-// @route   GET api/products
+// @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
@@ -13,7 +13,7 @@ const getProducts = asyncHandler(async (req, res) => {
 })
 
 // @desc    Fetch single product
-// @route   GET api/products/:id
+// @route   GET /api/products/:id
 // @access  Public
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id)
