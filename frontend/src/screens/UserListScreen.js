8d5223e2b554977e24c9d197b90a3a97162a9300
Charity Quinsay <71797778+cquinsay@users.noreply.github.com>
2022-02-12 11:51:43 -0800

    created user delete for admin

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -5,7 +5,7 @@ import { Table, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { listUsers } from '../actions/userActions';
+import { listUsers, deleteUser } from '../actions/userActions';
 
 const UserListScreen = () => {
   const dispatch = useDispatch()
@@ -17,6 +17,9 @@ const UserListScreen = () => {
   const userLogin = useSelector(state => state.userLogin)
   const { userInfo } = userLogin
 
+  const userDelete = useSelector(state => state.userDelete)
+  const { success: successDelete } = userDelete
+
 
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) {
@@ -26,10 +29,12 @@ const UserListScreen = () => {
       navigate('/login')
 
     }
-  }, [dispatch, navigate])
+  }, [dispatch, navigate, userInfo, successDelete])
 
   const deleteHandler = (id) => {
-    console.log('delete')
+    if (window.confirm('Are you sure?')) {
+      dispatch(deleteUser(id))
+    }
   }
 
   return <>
