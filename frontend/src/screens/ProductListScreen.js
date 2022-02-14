ce446c77341b225cb6af7e85fb36421257dfb3ba
Charity Quinsay <71797778+cquinsay@users.noreply.github.com>
2022-02-14 13:24:34 -0800

    added create product to frontend

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -5,7 +5,8 @@ import { Table, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { listProducts, deleteProduct } from '../actions/productActions';
+import { listProducts, deleteProduct, createProduct } from '../actions/productActions';
+import { PRODUCT_CREATE_RESET } from '../constants/productConstants';
 
 const ProductListScreen = () => {
   const dispatch = useDispatch()
@@ -17,20 +18,26 @@ const ProductListScreen = () => {
   const productDelete = useSelector(state => state.productDelete)
   const { loading: loadingDelete, error: errorDelete, success: successDelete } = productDelete
 
+  const productCreate = useSelector(state => state.productCreate)
+  const { loading: loadingCreate, error: errorCreate, success: successCreate, product: createdProduct } = productCreate
+
   const userLogin = useSelector(state => state.userLogin)
   const { userInfo } = userLogin
 
 
 
   useEffect(() => {
-    if (userInfo && userInfo.isAdmin) {
-      dispatch(listProducts())
-
-    } else {
+    dispatch({ type: PRODUCT_CREATE_RESET })
+    if (!userInfo.isAdmin) {
       navigate('/login')
 
     }
-  }, [dispatch, navigate, userInfo, successDelete])
+    if (successCreate) {
+      navigate(`/admin/product/${createdProduct._id}/edit`)
+    } else {
+      dispatch(listProducts())
+    }
+  }, [dispatch, navigate, userInfo, successDelete, successCreate, createdProduct])
 
   const deleteHandler = (id) => {
     if (window.confirm('Are you sure?')) {
@@ -38,8 +45,8 @@ const ProductListScreen = () => {
     }
   }
 
-  const createProductHandler = (product) => {
-    console.log("Create product")
+  const createProductHandler = () => {
+    dispatch(createProduct())
   }
 
   return <>
@@ -56,6 +63,8 @@ const ProductListScreen = () => {
     </Row>
     {loadingDelete && <Loader />}
     {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+    {loadingCreate && <Loader />}
+    {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
     {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
       <Table striped bordered hover responsive className='table-sm'>
         <thead>
